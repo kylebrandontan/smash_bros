ca0641fe19154c8bc8b518bbd994b7377895e65c
Kyle Tan <kylebrandontan@gmail.com>
2019-09-18 18:19:10 +0800

    add users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/new'
   # root 'matches#index', as: 'home'
   get 'matches', to: 'matches#index', as: 'matches'
+  get 'signup', to: 'users#new'
   root 'pages#about', as: 'home'
   resources :matches
 end
