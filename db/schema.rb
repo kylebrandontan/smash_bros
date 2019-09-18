b1a4585a866ddce3fc4268f76507438bf2a19b87
Kyle Tan <kylebrandontan@gmail.com>
2019-09-18 18:44:18 +0800

    make a User model with secure passwords

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_17_083727) do
+ActiveRecord::Schema.define(version: 2019_09_18_103600) do
 
   create_table "matches", force: :cascade do |t|
     t.string "fighter1"
@@ -18,6 +18,17 @@ ActiveRecord::Schema.define(version: 2019_09_17_083727) do
     t.string "winner"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "username"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
