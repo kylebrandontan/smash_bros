9996d2c0f178e5342b0497174e163db3ff85d98a
Kyle Tan <kylebrandontan@gmail.com>
2019-09-19 00:11:54 +0800

    finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/new'
   # root 'matches#index', as: 'home'
   get 'matches', to: 'matches#index', as: 'matches'
   root 'pages#about', as: 'home'
+  get  '/signup',  to: 'users#new'
   resources :matches
 end
