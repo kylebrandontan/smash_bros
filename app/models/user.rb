6c4ec55e37a58417c9fad9657f5fd3eb9a6efa44
Kyle Tan <kylebrandontan@gmail.com>
2019-09-24 17:42:06 +0800

    restored previous version

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,8 @@
 class User < ApplicationRecord
   before_save {self.email = email.downcase}
-  before_save {self.username = username.downcase}
   validates :name, presence: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}, uniqueness: true
-  validates :username, presence: true, uniqueness: true
   validates :password, presence: true, length: {minimum: 6}
   has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   get 'users/new'
   # root 'matches#index', as: 'home'
   get 'matches', to: 'matches#index', as: 'matches'
-  get 'signup', to: 'users#new'
   root 'pages#about', as: 'home'
   get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
   resources :matches
+  resources :users
 end
